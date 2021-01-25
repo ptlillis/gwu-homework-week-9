53d409df8a77c710f657aee405cc32b5d6849277
ptlillis <73671618+ptlillis@users.noreply.github.com>
2021-01-24 20:39:59 -0500

    Update generateMarkdown.js

--- readme homework/Develop/utils/generateMarkdown.js
+++ readme homework/Develop/utils/generateMarkdown.js
@@ -44,13 +44,13 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(title, licensing) {
-  var link = renderLicenseLink(licensing)
+function renderLicenseSection(title, license) {
+  var link = renderLicenseLink(license)
 
   if(link === '') {
     return ''
   } else {
-    return 'The  application is covered under the [${license}] license. More info: (${link})'
+    return 'The application is covered under the [${license}] license. More info: (${licensing})'
   }
 }
 
